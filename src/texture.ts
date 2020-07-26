84d2d36eb7ce162af380d406cd890ff984b67edb
Hugo <hugobastas@protonmail.com>
2020-07-26 14:35:28 +0200

    Refactor texture.ts

--- src/texture.ts
+++ src/texture.ts
@@ -1,44 +1,6 @@
-import { GPU } from "./gpu"
+import { Gpu } from "./index"
 
-export function newTexture(gpu: GPU, width: number, height: number): UnInitializedTexture {
-  return new Texture(gpu, width, height) as UnInitializedTexture
-}
-
-export function isUnInitializedTexture(v: any): v is UnInitializedTexture {
-  return v instanceof Texture && !v.isInitialized
-}
-
-export function isInitializedTexture(v: any): v is InitializedTexture {
-  return v instanceof Texture && v.isInitialized
-}
-
-export interface UnInitializedTexture {
-  width: number
-  height: number
-  isInitialized: false
-  gpu: GPU
-  glTexture: WebGLTexture
-  glFramebuffer: WebGLFramebuffer
-  fill(n?: number): InitializedTexture
-  uploadData(v: Uint8Array | ((col: number, row: number) => Pixel)): InitializedTexture
-  uploadData(v: Uint8Array | ((col: number, row: number) => Pixel), xOffset: number, yOffset: number, width: number, height: number): InitializedTexture
-}
-
-export interface InitializedTexture {
-  width: number
-  height: number
-  isInitialized: true
-  gpu: GPU
-  glTexture: WebGLTexture
-  glFramebuffer: WebGLFramebuffer
-  fill(n?: number): InitializedTexture
-  uploadData(v: Uint8Array | ((col: number, row: number) => Pixel)): InitializedTexture
-  uploadData(v: Uint8Array | ((col: number, row: number) => Pixel), xOffset: number, yOffset: number, width: number, height: number): InitializedTexture
-  downloadData(): Uint8Array
-  downloadData(data: Uint8Array): void
-  downloadData(x: number, y: number, width: number, height: number, data: Uint8Array): void
-  downloadData(x: number, y: number, width: number, height: number): Uint8Array
-}
+export type DataF = (col: number, row: number) => Pixel
 
 export type Pixel = {
   r: number,
@@ -47,167 +9,299 @@ export type Pixel = {
   a: number,
 }
 
-class Texture {
-  private _width: number
-  private _height: number
-  private _isInitialized: boolean
-  private _gpu: GPU
-  private _glTexture: WebGLTexture
-  private _glFramebuffer: WebGLFramebuffer
+export interface Texture {
+  _gpu: Gpu
+}
 
-  get width() { return this._width }
-  get height() { return this._height }
-  get isInitialized() { return this._isInitialized }
-  get gpu() { return this._gpu }
-  get glTexture() { return this._glTexture }
-  get glFramebuffer() { return this._glFramebuffer }
+export interface WithWidth {
+  _width: number
+}
 
-  constructor(gpu: GPU, width: number, height: number) {
-    this._width = width
-    this._height = height
-    this._isInitialized = false
-    this._gpu = gpu
+export interface WithHeight {
+  _height: number
+}
 
-    let gl = gpu._gl
+export interface WithGlTexture extends WithWidth, WithHeight {
+  _glTexture: WebGLTexture
+}
 
-    this._glTexture = gl.createTexture() as WebGLTexture
-    this._glFramebuffer = gl.createFramebuffer() as WebGLFramebuffer
+export interface WithoutGlTexture {
+  _glTexture?: never
+}
 
-    gl.bindTexture(gl.TEXTURE_2D, this._glTexture)
+export interface WithGlTextureInitialized extends WithGlTexture {
+  _textureInitialized: true
+}
 
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+export interface WithBoundFramebuffer extends WithGlTexture {
+  _glFramebuffer: WebGLFramebuffer
+}
+
+export interface WithoutBoundFrameBuffer {
+  _glFramebuffer?: never
+}
+
+export function texture(gpu: Gpu): Texture {
+  return { _gpu: gpu }
+}
+
+export type TextureFunc<I, O> = <T extends Texture>(t: T & I) => T & O
+
+export function width(w: number): TextureFunc<WithoutGlTexture, WithWidth> {
+  return function <T extends Texture>(t: T) {
+    let nt = t as T & WithWidth
+    nt._width = w
+   return nt
   }
+}
 
-  fill(n?: number): Texture {
-    if (typeof n === "undefined" || n === 0) {
-      let gl = this._gpu._gl
-
-      const target = gl.TEXTURE_2D
-      const format = gl.RGBA
-      const type = gl.UNSIGNED_BYTE
-
-      gl.bindTexture(target, this._glTexture)
-      gl.texImage2D(target, 0, format, this.width, this.height, 0, format, type, null)
-
-      if (!this.isInitialized) {
-        gl.bindFramebuffer(gl.FRAMEBUFFER, this._glFramebuffer)
-        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, this._glTexture, 0)
-        this._isInitialized = true
-      }
-
-      return this
-    }
-
-    this.uploadData(new Uint8Array(this.width * this.height * 4).fill(n || 0))
-    return this
+export function height(h: number): <T extends Texture>(t: T & WithoutGlTexture) => T & WithHeight {
+  return function <T extends Texture>(t: T) {
+    let nt = t as T & WithHeight
+    nt._height = h
+    return nt
   }
+}
 
-  uploadData(v: Uint8Array | ((col: number, row: number) => Pixel)): Texture
-  uploadData(v: Uint8Array | ((col: number, row: number) => Pixel), xOffset: number, yOffset: number, width: number, height: number): Texture
-  uploadData(v: Uint8Array | ((col: number, row: number) => Pixel), ...args: any[]): Texture {
-    let data: Uint8Array
-    if (typeof v === "function") {
-      data = new Uint8Array(this.width * this.height * 4)
-      for (let row = 0; row < this.height; row ++) {
-        for (let col = 0; col < this.width; col ++) {
-          let { r, g, b, a } = v(col, row)
-          let index = (row * this.width + col) * 4
-          data[index] = r
-          data[index + 1] = g
-          data[index + 2] = b
-          data[index + 3] = a
-        }
-      }
+export function createTexture<T extends Texture>(t: T & WithWidth & WithHeight & WithoutGlTexture): T & WithGlTexture {
+  let gl = t._gpu._gl
+
+  let glTexture = gl.createTexture() as WebGLTexture
+  gl.bindTexture(gl.TEXTURE_2D, glTexture)
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+  //@ts-ignore
+  let nt = t as T & WithGlTexture
+  nt._glTexture = glTexture
+  return nt
+}
+
+export function createBoundFramebuffer<T extends Texture>(t: T & WithGlTexture & WithoutBoundFrameBuffer): T & WithBoundFramebuffer {
+  let gl = t._gpu._gl
+
+  //@ts-ignore
+  let framebuffer: WebGLFramebuffer = gl.createFramebuffer()
+  gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer)
+  gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, t._glTexture, 0)
+
+  //@ts-ignore
+  let nt = t as T & WithBoundFramebuffer
+  nt._glFramebuffer = framebuffer
+  return nt
+}
+
+export function fill({r, g, b, a}: Pixel): <T extends Texture>(t: T & WithGlTexture) => T & WithGlTextureInitialized {
+  return function <T extends Texture & WithGlTexture>(t: T): T & WithGlTextureInitialized {
+    let data: null | Uint8Array | Uint32Array
+    if (r == 0 && g == 0 && b == 0 && a == 0) {
+      data = null
     } else {
-      data = v
+      let v = r + (g << 8) + (b << 16) + (a << 24)
+      data = new Uint32Array(t._width * t._height).fill(v)
     }
 
-    let gl = this._gpu._gl
+    let gl = t._gpu._gl
 
     const target = gl.TEXTURE_2D
     const format = gl.RGBA
     const type = gl.UNSIGNED_BYTE
 
-    gl.bindTexture(gl.TEXTURE_2D, this._glTexture)
+    gl.bindTexture(target, t._glTexture)
+    gl.texImage2D(target, 0, format, t._width, t._height, 0, format, type, data)
 
-    if (args.length === 1) {
-      if (data.length < this.width * this.height * 4)
-        throw new Error("Not enough data")
-
-      gl.texImage2D(target, 0, format, this.width, this.height, 0, format, type, data)
-    } else {
-      let [xOffset, yOffset, width, height] = args
-
-      if (data.length < width * height)
-        throw new Error("Not enough data")
-
-      if (!this.isInitialized)
-        throw new Error("Texture in not initialized")
-
-      gl.texSubImage2D(target, 0, xOffset, yOffset, width, height, format, type, data)
-    }
-
-    if (!this.isInitialized) {
-      gl.bindFramebuffer(gl.FRAMEBUFFER, this._glFramebuffer)
-      gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, this._glTexture, 0)
-      this._isInitialized = true
-    }
-
-    return this
+    let nt = t as T & WithGlTextureInitialized
+    nt._textureInitialized = true
+    return nt
   }
+}
 
-  downloadData(): Uint8Array
-  downloadData(data: Uint8Array): void
-  downloadData(x: number, y: number, width: number, height: number, data: Uint8Array): void
-  downloadData(x: number, y: number, width: number, height: number): Uint8Array
-  downloadData(...args: any): Uint8Array | undefined {
-    let _x, _y, _width, _height: number
-    let _data: Uint8Array
-    let _return: boolean
+export function uploadData(a: ArrayBufferView | DataF): <T extends Texture & WithGlTexture>(t: T) => T & WithGlTextureInitialized {
+  return function <T extends Texture & WithGlTexture>(t: T): T & WithGlTextureInitialized {
+    let data: ArrayBufferView
 
-    if (typeof args[0] === "undefined") {
-      [_x, _y, _width, _height] = [0, 0, this.width, this.height]
-      _data = new Uint8Array(this.width * this.height * 4)
-      _return = true
-    }
+    if (typeof a === "function") {
+      let u8Data = new Uint8Array(t._width * t._height * 4)
+      for (let col = 0; col < t._width; col++) {
+        for (let row = 0; row < t._height; row++) {
+          let i = (row * t._width + col) * 4
+          let pixel = a(col, row)
 
-    else if (args[0] instanceof Uint8Array) {
-      [_x, _y, _width, _height] = [0, 0, this.width, this.height]
-      _data = args[0];
-      _return = false
-    }
-
-    else if (typeof args[0] === "number") {
-      [_x, _y, _width, _height] = args
-
-      if (args[4]) {
-        _data = args[4]
-        _return = false
-      } else {
-        _data = new Uint8Array(_width * _height * 4)
-        _return = true
+          u8Data[i] = pixel.r
+          u8Data[i+1] = pixel.g
+          u8Data[i+2] = pixel.b
+          u8Data[i+3] = pixel.a
+        }
       }
+
+      data = u8Data
+    } else {
+      let expectedBytes = t._width * t._height * 4
+      if (a.byteLength != expectedBytes) {
+        throw new Error(`Invalid data length: Expected ${expectedBytes} bytes, got ${a.byteLength} bytes.`)
+      }
+
+      data = a
     }
 
-    else {
-      throw new TypeError()
-    }
+    let gl = t._gpu._gl
 
-    let gl = this._gpu._gl
-
+    const target = gl.TEXTURE_2D
     const format = gl.RGBA
     const type = gl.UNSIGNED_BYTE
 
-    gl.bindFramebuffer(gl.FRAMEBUFFER, this._glFramebuffer)
-    gl.readPixels(_x, _y, _width, _height, format, type, _data);
+    gl.bindTexture(target, t._glTexture)
+    gl.texImage2D(target, 0, format, t._width, t._height, 0, format, type, data)
 
-    if (_return)
-      return _data
+    let nt = t as T & WithGlTextureInitialized
+    nt._textureInitialized = true
+    return nt
+  }
+}
 
-    return
+export function uploadPartialData(
+  a: ArrayBufferView | DataF,
+  x: number,
+  y: number,
+  width: number,
+  height: number
+): <T extends Texture & WithGlTextureInitialized>(t: T) => T {
+  return function <T extends Texture & WithGlTextureInitialized>(t: T): T {
+    if (x + width > t._width)
+      throw new Error("Invalid x and/or width")
+
+    if (y + height > t._height)
+      throw new Error("Invalid y and/or height")
+
+    let data: ArrayBufferView
+
+    if (typeof a === "function") {
+      let u8Data = new Uint8Array(width * height * 4)
+      for (let col = 0; col < width; col++) {
+        for (let row = 0; row < height; row++) {
+          let i = (row * t._width + col) * 4
+          let pixel = a(col, row)
+
+          u8Data[i] = pixel.r
+          u8Data[i+1] = pixel.g
+          u8Data[i+2] = pixel.b
+          u8Data[i+3] = pixel.a
+        }
+      }
+
+      data = u8Data
+    } else {
+      let expectedBytes = width * height * 4
+      if (a.byteLength != expectedBytes) {
+        throw new Error(`Invalid data length: Expected ${expectedBytes} bytes, got ${a.byteLength} bytes.`)
+      }
+
+      data = a
+    }
+
+    let gl = t._gpu._gl
+
+    const target = gl.TEXTURE_2D
+    const format = gl.RGBA
+    const type = gl.UNSIGNED_BYTE
+
+    gl.bindTexture(target, t._glTexture)
+    gl.texSubImage2D(target, 0, x, y, width, height, format, type, data)
+
+    return t
+  }
+}
+
+export function downloadData(t: Texture & WithBoundFramebuffer): ArrayBuffer {
+  let arrayBufferView = new Uint8Array(t._width * t._height * 4)
+  downloadSubDataTo_(t, arrayBufferView, 0, 0, t._width, t._height)
+  return arrayBufferView.buffer
+}
+
+export function downloadSubData(
+  x: number, y: number, width: number, height: number
+): (t: Texture & WithBoundFramebuffer) => ArrayBuffer {
+  return t => {
+    verifyWithinBounds(t, x, y, width, height)
+
+    let arrayBufferView = new Uint8Array(width * height * 4)
+    downloadSubDataTo_(t, arrayBufferView, x, y, width, height)
+    return arrayBufferView.buffer
+  }
+}
+
+export function downloadDataTo(d: ArrayBufferView): (t: Texture & WithBoundFramebuffer) => void {
+  return t => {
+    verifyBufferViewSize(d, t._width * t._height * 4)
+
+    downloadSubDataTo_(t, d, 0, 0, t._width, t._height)
+  }
+}
+
+export function downloadSubDataTo(
+  x: number, y: number, width: number, height: number, d: ArrayBufferView
+): (t: Texture & WithBoundFramebuffer) => void {
+  return t => {
+    verifyWithinBounds(t, x, y, width, height)
+    verifyBufferViewSize(d, width * height * 4)
+
+    downloadSubDataTo_(t, d, x, y, width, height)
+  }
+}
+
+function downloadSubDataTo_(
+  t: Texture & WithBoundFramebuffer,
+  d: ArrayBufferView,
+  x: number,
+  y: number,
+  width: number,
+  height: number
+) {
+  let gl = t._gpu._gl
+
+  const format = gl.RGBA
+  const type = gl.UNSIGNED_BYTE
+
+  gl.bindFramebuffer(gl.FRAMEBUFFER, t._glFramebuffer)
+  gl.readPixels(x, y, width, height, format, type, d);
+}
+
+function verifyBufferViewSize(view: ArrayBufferView, expectedBytes: number) {
+  if (view.byteLength != expectedBytes)
+    throw new InvalidBufferViewSize(view, expectedBytes)
+}
+
+class InvalidBufferViewSize extends Error {
+  constructor(view: ArrayBufferView, expectedBytes: number) {
+    super(`Invalid BufferView size. Expected ${expectedBytes} bytes, is ${view.byteLength} bytes`)
+  }
+}
+
+function verifyWithinBounds(
+  t: WithWidth & WithHeight,
+  x: number,
+  y: number,
+  width: number,
+  height: number
+) {
+  if (x + width > t._width)
+    throw new InvalidXAndOrWidth()
+
+  if (y + height > t._height)
+    throw new InvalidYAndOrHeight()
+}
+
+class InvalidXAndOrWidth extends Error {
+  constructor() {
+    super("Invalid x and/or width")
+  }
+}
+
+class InvalidYAndOrHeight extends Error {
+  constructor() {
+    super("Invalid y and/or height")
   }
 }
 
