fd590d677253e1d3621e2377b62df8a1dbc5764a
Hugo <hugobastas@protonmail.com>
2020-07-26 19:38:52 +0200

    Support pipeline operators when testing with Jest

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   presets: [
-    [ '@babel/preset-env', {targets: {node: 'current'}} ],
+    [ "@babel/preset-env", { targets: { node: "current" } } ],
     "@babel/preset-typescript",
   ],
+  plugins: [
+    [ "@babel/plugin-proposal-pipeline-operator", { proposal: "minimal" } ]
+  ]
 };
